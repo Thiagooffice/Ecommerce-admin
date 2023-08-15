67eb4ae5a6a253132005e8f01a6f2fbc2fd8963b
Thiago <thiagooffice2@gmail.com>
2023-08-15 20:54:06 -0300

    feat: creating edit products page

--- models/Products.js
+++ models/Products.js
@@ -1,9 +1,9 @@
-import {Schema, model} from "mongoose"
+import { Schema, model, models } from "mongoose"
 
 const ProductSchema = new Schema({
-    title: { type:String, required: true },
+    title: { type: String, required: true },
     description: String,
-    price: {type: Number, required: true},
+    price: { type: Number, required: true },
 })
 
-export const Product = model("product", ProductSchema)
+export const Product = models.Product || model("Product", ProductSchema)
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,16 +1,29 @@
-
+import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Products";
-import mongoose from "mongoose";
 
 export default async function handler(req, res){
     const {method} = req;
+    mongooseConnect()
 
+    if(method === "GET"){
+        if(req.query?.id){
+            res.json(await Product.findOne({_id: req.query?.id}))
+        }else {
+            res.json(await Product.find());
+        }
+    }
 
     if(method === "POST"){
-        const {title, description, rpice} = req.body
+        const {title, description, price} = req.body
         const productDoc = await Product.create({
-            title, description,price
+            title, description, price
         })
         res.json(productDoc)
+    }
+
+    if(method === "PUt"){
+        const {title, description, price, _id} = req.body
+        await Product.updateOne({_id}, {title, description, price});
+        res.json(true)
     }
 }
